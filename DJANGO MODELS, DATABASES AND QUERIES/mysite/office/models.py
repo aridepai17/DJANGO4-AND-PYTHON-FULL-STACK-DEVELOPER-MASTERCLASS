5b556d5103f9e446b31db0cd4e05eead69085e17
aridepai17 <advaithdepai26@gmail.com>
2024-08-25 10:28:47 +0530

    Basic essentials

--- DJANGO MODELS, DATABASES AND QUERIES/mysite/office/models.py
+++ DJANGO MODELS, DATABASES AND QUERIES/mysite/office/models.py
@@ -5,3 +5,7 @@ class Patient(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
     age = models.IntegerField()
+    
+    def __str__(self):
+        
+        return f"{self.firstname} {self.lastname} {self.age}"
