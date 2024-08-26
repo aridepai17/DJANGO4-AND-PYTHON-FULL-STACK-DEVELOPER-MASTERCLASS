0c9d3c3281c14dd5e7c78c75977600e2c8b676f8
aridepai17 <advaithdepai26@gmail.com>
2024-08-26 20:33:51 +0530

    Essentials

--- DJANGO MODELS, DATABASES AND QUERIES/mysite/office/models.py
+++ DJANGO MODELS, DATABASES AND QUERIES/mysite/office/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Patient(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
-    age = models.IntegerField()
+    age = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(120)])
+    heartrate = models.IntegerField(default=60, validators = [MinValueValidator(1), MaxValueValidator(300)])
     
     def __str__(self):
         
--- DJANGO MODELS, DATABASES AND QUERIES/mysite/office/views.py
+++ DJANGO MODELS, DATABASES AND QUERIES/mysite/office/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+def listpatients(request):
+    allpatients = models.Patient.objects.all()
+    context = {'patients':allpatients}
+    return render(request, 'office/list.html', context = context)
