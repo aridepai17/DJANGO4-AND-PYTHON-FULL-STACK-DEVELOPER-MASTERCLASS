9188d378bf5b6bcdf7a1aa6617804205a8fa4358
aridepai17 <advaithdepai26@gmail.com>
2024-09-01 18:25:21 +0530

    Modified views.py

--- USER AUTHENTICATION AND SESSIONS/library/catalog/views.py
+++ USER AUTHENTICATION AND SESSIONS/library/catalog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance, Genre, Language
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the catalog index.")
+    numbooks = Book.objects.all().count()
+    numinstances = BookInstance.objects.all().count()  
+    
+    numinstancesavail = BookInstance.objects.filter(status__exact='a').count()
+    
+    context = {'numbooks':numbooks, 'numinstances':numinstances, 'numinstancesavail':numinstancesavail}
+    return render(request, 'index.html', context = context )
